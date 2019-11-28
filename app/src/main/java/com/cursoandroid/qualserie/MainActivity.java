be4330321a0075876af0366c96e5009e1f3874ae
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-27 21:15:36 -0300

    Adding dummy event listener.

--- app/src/main/java/com/cursoandroid/qualserie/MainActivity.java
+++ app/src/main/java/com/cursoandroid/qualserie/MainActivity.java
@@ -20,5 +20,22 @@ public class MainActivity extends AppCompatActivity {
 
         humor = findViewById(R.id.humor);
 
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
