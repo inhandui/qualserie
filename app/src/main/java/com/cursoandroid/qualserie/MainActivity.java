c98e3a64058b5ed40e68e880f93e52f1c96cb244
Inhandui Barbosa Ferreira <inhandui@gmail.com>
2019-11-27 21:11:41 -0300

    Adding seekbar and image references.

--- app/src/main/java/com/cursoandroid/qualserie/MainActivity.java
+++ app/src/main/java/com/cursoandroid/qualserie/MainActivity.java
@@ -3,12 +3,22 @@ package com.cursoandroid.qualserie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private ImageView humor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = findViewById(R.id.seekbar);
+
+        humor = findViewById(R.id.humor);
+
     }
 }
